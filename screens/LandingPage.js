87b340d26dfd53ff5bb4b001304afb9f903e889c
Somers Matthews <somersbmatthews@gmail.com>
2019-12-05 08:43:29 -0600

    installed jest and storybook

---
+++ screens/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const LandingPage = props => {
+   return (
+      <View>
+
+      </View>
+   );
+};
+
+const styles = StyleSheet.create({
+   screen: {
+      flex: 1,
+      padding: 10,
+      alignItems: "center"
+   }
+});
+
+export default LandingPage;
